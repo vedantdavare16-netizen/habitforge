8818e549a24c0ebe4b3a6db8f1785455e8140008
vedantdavare16-netizen <vedantdavare16@gmail.com>
2026-03-21 22:38:48 +0530

    updated API base URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://habitforge-backend-biji.onrender.com"
 });
-
 // ✅ Attach token to every request
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
